63b81c8511f9590cad65a11160cc4489bc55e343
apicov <a.pico@posteo.de>
2025-07-23 16:39:21 +0200

    enhance wsgi.py to activate Conda environment and set application path

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
 import sys
 import os
 
+# Activate the Conda environment
+activate_this = '/home/pico/miniconda3/envs/lino/bin/activate_this.py'
+exec(open(activate_this).read(), dict(__file__=activate_this))
+
+# Add the path to your application
 sys.path.insert(0, '/var/www/apicov.xyz')
 
 # Set the Flask app environment variable
